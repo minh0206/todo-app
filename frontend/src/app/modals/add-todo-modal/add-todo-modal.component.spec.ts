9221ddd6bf24ffa9a35319063cc6e2dbcea276db
Minh <minh020698@gmail.com>
2024-04-18 20:44:43 +0700

    feat(frontend): add add-todo-modal

---
+++ frontend/src/app/modals/add-todo-modal/add-todo-modal.component.spec.ts
@@ -0,0 +1,27 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { AddTodoModalComponent } from './add-todo-modal.component';
+
+describe('AddTodoModalComponent', () => {
+  let component: AddTodoModalComponent;
+  let fixture: ComponentFixture<AddTodoModalComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddTodoModalComponent],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddTodoModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/src/app/modals/add-todo-modal/add-todo-modal.component.ts
@@ -0,0 +1,58 @@
+import { Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { Section } from '../../models/section';
+import { Todo } from '../../models/todo';
+import { TodoService } from '../../services/todo.service';
+
+@Component({
+  selector: 'app-add-todo-modal',
+  templateUrl: './add-todo-modal.component.html',
+  styleUrls: ['./add-todo-modal.component.css'],
+})
+export class AddTodoModalComponent {
+  activeModal = inject(NgbActiveModal);
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  name!: string;
+  description!: string;
+  selectedSection: string = 'inbox';
+  selectedProjectId = 1;
+  selectedSectionId: number | null = null;
+
+  projects: Project[] = [];
+
+  constructor(
+    projectService: ProjectService,
+    private todoService: TodoService
+  ) {
+    projectService.getProjects().subscribe((projects) => {
+      this.projects = projects;
+    });
+  }
+
+  selectProject(project: Project) {
+    this.selectedProjectId = project.id;
+    this.selectedSection = project.name;
+  }
+
+  selectProjectSection(project: Project, section: Section) {
+    this.selectedProjectId = project.id;
+    this.selectedSectionId = section.id;
+    this.selectedSection = project.name + ' - ' + section.name;
+  }
+
+  addTodo() {
+    const todo: Todo = {
+      project: this.selectedProjectId,
+      section: this.selectedSectionId,
+      name: this.name,
+      description: this.description,
+      done: false,
+    };
+
+    this.todoService.addTodo(todo);
+    this.activeModal.close();
+  }
+}
