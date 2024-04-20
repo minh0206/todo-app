61c730fc790a5bf74b0fd258d9c29f5abd51a8ec
Minh <minh020698@gmail.com>
2024-04-20 19:30:42 +0700

    refactor(frontend): change app routing and import

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InboxComponent } from './inbox/inbox.component';
-import { TodayComponent } from './today/today.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/inbox', pathMatch: 'full' },
-  { path: 'inbox', component: InboxComponent },
-  { path: 'today', component: TodayComponent },
-];
+const routes: Routes = [{ path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,30 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideBarComponent } from './side-bar/side-bar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { InboxComponent } from './inbox/inbox.component';
-import { TodayComponent } from './today/today.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SideBarComponent,
-    InboxComponent,
-    TodayComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    FormsModule,
-    DragDropModule,
-    BrowserAnimationsModule,
-  ],
+  imports: [BrowserModule, AuthModule, HomeModule, AppRoutingModule],
+  declarations: [AppComponent],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
